66fb6bdcf7a49f30d04caaa628a2c403e608090d
lucastso <lucastassisouza@gmail.com>
2022-02-28 21:57:59 -0300

    feat: add social icons in nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./styles.css";
+import github from "../../assets/github.svg";
+import insta from "../../assets/insta.svg";
+import linkedin from "../../assets/linkedin.svg";
 
 export function Nav() {
   return (
@@ -9,17 +12,42 @@ export function Nav() {
         <a href="/dev" className="nav-opt">
           Dev
         </a>
-        <a href="/dev" className="nav-opt">
+        <a href="/design" className="nav-opt">
           Design
         </a>
-        <a href="https://opensea.io/collection/paradox-ghosts" target="_blank">
+        <a
+          href="https://opensea.io/collection/paradox-ghosts"
+          target="_blank"
+          rel="noreferrer"
+        >
           NFT
         </a>
       </div>
       <div className="second-nav-items">
-        <div className="media-box1"></div>
-        <div className="media-box2"></div>
-        <div className="media-box3"></div>
+        <a
+          href="https://github.com/lucastso"
+          className="media-box1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} alt="github icon" />
+        </a>
+        <a
+          href="https://www.instagram.com/lucastassss/"
+          className="media-box2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={insta} alt="instagram icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lucas-tassi-souza-235489161/"
+          className="media-box3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="linkedin icon" />
+        </a>
       </div>
     </div>
   );
